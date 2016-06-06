83780956a02b24b0b0bce52e6f753180ab71dff2
Smirnov <smiranton.kiev@gmail.com>
2016-06-06 14:48:38 +0300

    - implemented render styleItems page

--- public/js/router.js
+++ public/js/router.js
@@ -10,23 +10,25 @@ define([
 
 ], function (Backendless, Backbone, Models) {
     var Router = Backbone.Router.extend({
-        
-        initialize: function () { },
-        
+
+        initialize: function () {
+        },
+
         routes: {
             'login'    : 'loginRout',
             'registr'  : 'registrationRout',
             'users'    : 'usersRout',
+            'styles'   : 'styleItemsRout',
             'retailers': 'retailerRout',
             '*any'     : 'anyRout'
         },
-        
+
         anyRout: function () {
             Backbone.history.navigate('users', {trigger: true})
         },
 
         retailerRout: function () {
-            var self = this;
+            var self  = this;
             var query = new Backendless.DataQuery();
 
             query.options = {relations: ['trendingStyles', 'contentCards']};
@@ -35,7 +37,7 @@ define([
 
                 Backendless.Persistence.of(Models.Retailer).find(query, new Backendless.Async(
                     function (list) {
-                        var dataList = list.data;
+                        var dataList           = list.data;
                         var retailerCollection;
                         var RetailerCollection = Backbone.Collection.extend({
                             model: Backbone.Model.extend({
@@ -71,13 +73,13 @@ define([
 
                 userStorage = Backendless.Persistence.of(Models.User);
 
-                queryData = new Backendless.DataQuery();
+                queryData           = new Backendless.DataQuery();
                 queryData.condition = "isAdmin = false";
-                queryData.options = { pageSize : 50, relations: ["followedRetailers"] };
+                queryData.options   = {pageSize: 50, relations: ["followedRetailers"]};
 
                 userStorage.find(queryData, new Backendless.Async(
                     function (list) {
-                        var dataList = list.data;
+                        var dataList       = list.data;
                         var userCollection;
                         var UserCollection = Backbone.Collection.extend({
                             model: Backbone.Model.extend({
@@ -103,7 +105,41 @@ define([
                 Backbone.history.navigate('login', {trigger: true});
             }
         },
-        
+
+        styleItemsRout: function () {
+            var self = this;
+            var styleItemsStorage;
+            var queryData;
+
+            if (APP.sessionData.get('authorized')) {
+                queryData         = new Backendless.DataQuery();
+                queryData.options = {pageSize: 50};
+
+                styleItemsStorage = Backendless.Persistence.of(Models.Style);
+                styleItemsStorage.find(queryData, new Backendless.Async(
+                    function (list) {
+                        var dataList             = list.data;
+                        var StyleItemsCollection = Backbone.Collection.extend({
+                            model: Backbone.Model.extend({
+                                'idAttribute': 'objectId'
+                            })
+                        });
+                        var styleItemsCollection  = new StyleItemsCollection(dataList);
+
+                        require(['views/styleItem/styleItemListView'], function (View) {
+                            self.wrapperView ? self.wrapperView.undelegateEvents() : null;
+                            self.wrapperView = new View({collection: styleItemsCollection});
+                        })
+
+                    },
+                    APP.errorHandler
+                ));
+
+            } else {
+                Backbone.history.navigate('login', {trigger: true});
+            }
+        },
+
         registrationRout: function () {
             if (!APP.sessionData.get('authorized')) {
                 require(['views/registration/registrationView'], function (View) {
@@ -130,8 +166,8 @@ define([
                 Backbone.history.navigate('users', {trigger: true})
             }
         }
-        
+
     });
-    
+
     return Router;
 });
