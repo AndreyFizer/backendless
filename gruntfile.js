503a7a1d0da3a83ee4c936077c5b31d274513347
AndreyFizer <fizer.ap@outlook.com>
2016-06-06 17:55:34 +0300

    relations

--- gruntfile.js
+++ gruntfile.js
@@ -2,17 +2,17 @@
  * Created by andrey on 06.06.2016.
  */
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
     grunt.initConfig({
         
-        concat: {
-            dist: {
-                src : [
-                    'public/sass/*.scss'
-                ],
-                dest: 'public/sass/main.scss'
-            }
-        },
+        // concat: {
+        //     dist: {
+        //         src : [
+        //             'public/sass/*.scss'
+        //         ],
+        //         dest: 'public/sass/main.scss'
+        //     }
+        // },
         
         sass: {
             options: {
@@ -23,12 +23,20 @@ module.exports = function(grunt) {
                     'public/css/main.css': 'public/sass/main.scss'
                 }
             }
+        },
+        
+        watch: {
+            sass: {
+                files: 'public/sass/main.scss',
+                tasks: ['sass']
+            }
         }
     });
     
-    grunt.loadNpmTasks('grunt-contrib-concat');
+    // grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-sass');
     grunt.loadNpmTasks('grunt-notify');
     
-    grunt.registerTask('default', ['concat', 'sass']);
+    grunt.registerTask('default', ['sass', 'watch:sass']);
 };
