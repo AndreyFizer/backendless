db95adb3ede307a3d496cfa3a6dd12cd75031414
Smirnov <smiranton.kiev@gmail.com>
2016-06-08 16:39:08 +0300

    - refactored dialog buttons

--- public/js/views/styleItem/styleCreateView.js
+++ public/js/views/styleItem/styleCreateView.js
@@ -114,20 +114,16 @@ define([
                 }.bind(this),
                 buttons      : [
                     {
-                        text : "Cancel",
-                        icons: {
-                            primary: "ui-icon-closethick"
-                        },
-                        click: function () {
+                        text   : "Cancel",
+                        'class': 'btn btnMedium btnError',
+                        click  : function () {
                             $(this).dialog("close");
                         }
                     },
                     {
-                        text : "Save",
-                        icons: {
-                            primary: "ui-icon-check"
-                        },
-                        click: function () {
+                        text   : "Save",
+                        'class': 'btn btnMedium btnSuccess',
+                        click  : function () {
                             this.letsSaveStyleItem();
                         }.bind(this)
                     }
